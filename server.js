ae46669aacf730962297cfb14a48fd58bd694530
sathyaprakash195 <sathyaprakash195@gmail.com>
2021-04-21 15:30:58 +0530

    coomit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV ==='production')
 
     app.get('*' , (req , res)=>{
 
-        res.sendFile(path.resolve(__dirname  , '/client/build/index.html'))
+        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
 
     })
 }
